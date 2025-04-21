67bcf60a7c397daf8ed195dda4d8d5e0209e82f3
Duc Phu <nguyenducphu2308@gmail.com>
2025-04-21 22:11:48 +0700

    add doctor statistics

--- src/Pages/Doctor/Dashboard/Dashboard.jsx
+++ src/Pages/Doctor/Dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import './Dashboard.css';
 import DashboardHeader from "./DasheboardHeader";
 import PatientCard from "./PatientCard";
 import RecordResultModal from "./RecordResultModal";
+import DoctorStatiscsCard from "./DoctorStatisticsCard";
 
 const Dashboard = () => {
     const { isRecordResultModalOpen } = useSelector((state) => state.doctorDashboard);
@@ -15,6 +16,7 @@ const Dashboard = () => {
             <div className="dashboard-doctor-container">
                 <AppointmentList />
                 <PatientCard />
+                <DoctorStatiscsCard />
             </div>
         </DoctorLayout>
     );
